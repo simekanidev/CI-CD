95417afc1afab353ff0bfbb2de470a072816122c
simekani <simekani.mabmabe@gmail.com>
2024-04-20 22:33:51 +0200

    Clean up

--- SampleApp/SampleApp/ViewController.swift
+++ SampleApp/SampleApp/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        adfasfasdf
+        
         // Do any additional setup after loading the view.
     }
 
