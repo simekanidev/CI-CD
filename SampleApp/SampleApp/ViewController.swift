806637cd82046f1d2170aa02bde4657a03e79fdd
simekani <simekani.mabmabe@gmail.com>
2024-04-20 08:46:58 +0200

    Sample app

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Simekani Mabambi on 2024/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
