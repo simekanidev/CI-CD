e4ec29fa31aa0b1de68cfa4a68a617d8b6bd64f7
simekani <simekani.mabmabe@gmail.com>
2024-04-23 15:24:07 +0200

    CI/CD Pipeline

---
+++ SampleApp/SampleAppTests/SampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppTests.swift
+//  SampleAppTests
+//
+//  Created by Simekani Mabambi on 2024/04/23.
+//
+
+import XCTest
+@testable import SampleApp
+
+final class SampleAppTests: XCTestCase {
+
+    func testNothing() {
+        XCTAssert(true)
+    }
+
+}
